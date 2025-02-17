ed843bd2c6e7502edc0a62d403c96968e07e4f84
Justin Chung <justinchung@Justins-MacBook-Pro-3.local>
2025-02-17 18:39:17 -0500

    adding M2 HW baseline files

---
+++ public_html/M2/problem4.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once "base.php";
+
+$ucid = "mt85"; // <-- set your ucid
+
+
+$array1 = ["hello world!", "php programming", "special@#$%^&characters", "numbers 123 456", "mIxEd CaSe InPut!"];
+$array2 = ["hello world", "php programming", "this is a title case test", "capitalize every word", "mixEd CASE input"];
+$array3 = ["  hello   world  ", "php    programming  ", "  extra    spaces  between   words   ",
+    "      leading and trailing spaces      ", "multiple      spaces"];
+$array4 = ["hello world", "php programming", "short", "a", "even"];
+
+
+function transformText($arr, $arrayNumber) {
+    // Only make edits between the designated "Start" and "End" comments
+    printArrayInfoBasic($arr, $arrayNumber);
+
+    // Challenge 1: Remove non-alphanumeric characters except spaces
+    // Challenge 2: Convert text to Title Case
+    // Challenge 3: Trim leading/trailing spaces and remove duplicate spaces
+    // Result 1-3: Assign final phrase to `$placeholderForModifiedPhrase`
+    // Challenge 4 (extra credit): Extract middle 3 characters (beginning starts at middle of phrase),
+    // assign to `$placeholderForMiddleCharacters`
+    // if not enough characters assign "Not enough characters"
+
+    // Step 1: sketch out plan using comments (include ucid and date)
+    // Step 2: Add/commit your outline of comments (required for full credit)
+    // Step 3: Add code to solve the problem (add/commit as needed)
+    $placeholderForModifiedPhrase = "";
+    $placeholderForMiddleCharacters = "";
+    foreach ($arr as $index => $text) {
+        // Start Solution Edits
+
+        
+
+        // End Solution Edits
+
+        printStringTransformations($index, $placeholderForModifiedPhrase, $placeholderForMiddleCharacters);
+    }
+
+    echo "<br>______________________________________<br>";
+}
+
+// Run the problem
+printHeader($ucid, 4);
+transformText($array1, 1);
+transformText($array2, 2);
+transformText($array3, 3);
+transformText($array4, 4);
+printFooter($ucid, 4);
+
+?>
