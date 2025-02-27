79723bf775c8182a3ac8b0a51e1a36a6df473db6
Justin Chung <justinchung@Justins-MacBook-Pro-3.local>
2025-02-27 11:26:36 -0500

    Project setup

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -1,3 +1,6 @@
+<?php
+require(__DIR__ . "/../../lib/functions.php");
+?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -22,13 +25,50 @@
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if ((isset($_POST["email"])) && isset($_POST["password"]) && isset($_POST["confirm"]))
- {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $confirm = $_POST["confirm"];
+// TODO 2: add PHP Code
+if (
+    isset($_POST["email"])
+    && isset($_POST["password"]) && isset($_POST["confirm"])
+) {
 
- }
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
+    $confirm = se($_POST, "confirm", "", false);;
+    // TODO 3: validate/use
+    $hasError = false;
+    // Sanitize and validate email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address";
+        $hasError = true;
+    }
+    if (empty($email)) {
+        echo "Email must not be empty";
+        $hasError = true;
+    }
 
+    if (empty($password)) {
+        echo "Password must not be empty";
+        $hasError = true;
+    }
+
+    if (empty($confirm)) {
+        echo "Confirm password must not be empty";
+        $hasError = true;
+    }
+
+    if (strlen($password) < 8) {
+        echo "Password too short";
+        $hasError = true;
+    }
+
+    if ($password !== $confirm) {
+        echo "Passwords must match";
+        $hasError = true;
+    }
+
+    if (!$hasError) {
+        echo "Welcome, $email";
+    }
+}
 ?>
