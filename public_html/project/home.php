cbecb7f657567f8fa4d206a080d90fb41d53e2a4
Justin Chung <justinchung@pf2an35n.njitdm.campus.njit.edu>
2025-03-26 12:28:24 -0400

    flash stuff

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -3,11 +3,18 @@ require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-error_log("Session: ". var_export($_SESSION, true));
 if(is_logged_in()){
- echo "Welcome, " . get_user_email(); 
+    //flash("Welcome, " . get_user_email());
+}
+else{
+    flash("You're not logged in");
+}
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ echo "Welcome, " . $_SESSION["user"]["email"]; 
 }
 else{
   echo "You're not logged in";
-}
+}*/
+
+require(__DIR__."/../../partials/flash.php");
 ?>
